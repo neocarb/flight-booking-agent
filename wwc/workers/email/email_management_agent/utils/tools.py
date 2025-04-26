1c5e4e75ab407885d4d4d48b85fce75cfce31ab4
neocarb <external@neocarbtechnology.com>
2025-04-27 01:24:34 +0800

    add token

--- wwc/workers/email/email_management_agent/utils/tools.py
+++ wwc/workers/email/email_management_agent/utils/tools.py
@@ -8,10 +8,11 @@ from langchain_google_community.gmail.utils import (
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 credentials_path = os.path.join(BASE_DIR, "credentials.json")
+token_path = os.path.join(BASE_DIR, "token.json")
 print(f"Credentials path: {credentials_path}")
 
 credentials = get_gmail_credentials(
-    token_file="token.json",
+    token_file=token_path,
     scopes=["https://mail.google.com/"],
     client_secrets_file=credentials_path
 )
