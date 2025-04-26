cbe981ef381d863d4a817ec39a57dd0e1f5d931f
neocarb <external@neocarbtechnology.com>
2025-04-27 01:15:50 +0800

    change path creds

--- wwc/workers/email/email_management_agent/utils/tools.py
+++ wwc/workers/email/email_management_agent/utils/tools.py
@@ -1,13 +1,19 @@
+import os
+
 from langchain_google_community import GmailToolkit
 from langchain_google_community.gmail.utils import (
     build_resource_service,
     get_gmail_credentials,
 )
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+credentials_path = os.path.join(BASE_DIR, "credentials.json")
+print(f"Credentials path: {credentials_path}")
+
 credentials = get_gmail_credentials(
     token_file="token.json",
     scopes=["https://mail.google.com/"],
-    client_secrets_file="credentials.json",
+    client_secrets_file=credentials_path
 )
 api_resource = build_resource_service(credentials=credentials)
 email_toolkit = GmailToolkit(api_resource=api_resource)
