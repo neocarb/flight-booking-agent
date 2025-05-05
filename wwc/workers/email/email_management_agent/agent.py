0cd7a2e098b85a999532f18edea899c8fd931eef
neocarb <external@neocarbtechnology.com>
2025-05-05 22:39:41 +0800

    abs package

--- wwc/workers/email/email_management_agent/agent.py
+++ wwc/workers/email/email_management_agent/agent.py
@@ -1,8 +1,8 @@
 from typing import TypedDict, Literal
 
 from langgraph.graph import StateGraph, START, END
-from email_management_agent.utils.state import EmailManagerState
-from email_management_agent.utils.nodes import email_manager_node, email_human_node
+from wwc.workers.email.email_management_agent.utils.state import EmailManagerState
+from wwc.workers.email.email_management_agent.utils.nodes import email_manager_node, email_human_node
 
 email_manager_builder = StateGraph(EmailManagerState)
 email_manager_builder.add_node("email_manager_node", email_manager_node)
