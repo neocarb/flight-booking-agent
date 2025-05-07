5e93eaa401960b3661ed241180ac89a32ac60cc6
neocarb <external@neocarbtechnology.com>
2025-05-07 15:18:19 +0800

    add flight_booking_agent to sup

--- wwc/supervisor/agent.py
+++ wwc/supervisor/agent.py
@@ -1,11 +1,12 @@
 from langchain_openai import ChatOpenAI
 from langgraph_supervisor import create_supervisor
 from wwc.workers.email_agent.agent import email_agent
+from wwc.workers.flight_agents.flight_booking_agent.agent import flight_booking_agent
 
 model = ChatOpenAI(model="gpt-4o")
 # Create supervisor workflow
 workflow = create_supervisor(
-    agents=[email_agent],
+    agents=[email_agent, flight_booking_agent],
     model=model,
     prompt=(
         "You are a team supervisor managing a flight_booking_graph and email_manager_graph as tools"
