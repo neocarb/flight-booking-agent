43c6c0760f181a5d8a6ad0a39e2320f37130c434
neocarb <external@neocarbtechnology.com>
2025-05-07 21:40:49 +0800

    add checkpointer

--- wwc/workers/email_agent/agent.py
+++ wwc/workers/email_agent/agent.py
@@ -1,10 +1,11 @@
 from langgraph.graph import StateGraph, START
 from wwc.workers.email_agent.utils.state import EmailManagerState
 from wwc.workers.email_agent.utils.nodes import email_agent_node
+from langgraph.checkpoint.memory import MemorySaver
 
 email_agent_builder = StateGraph(EmailManagerState)
 email_agent_builder.add_node("email_agent_node", email_agent_node)
 email_agent_builder.add_edge(START, "email_agent_node")
 
-# checkpointer = MemorySaver()
-email_agent = email_agent_builder.compile(name="email_agent")
+checkpointer = MemorySaver()
+email_agent = email_agent_builder.compile(checkpointer=checkpointer, name="email_agent")
