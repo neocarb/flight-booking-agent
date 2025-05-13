63926a361b9dc23c5428c79239127c127a06f7a6
neocarb <external@neocarbtechnology.com>
2025-05-13 14:42:25 +0800

    change strategy to use tool call to judge success

--- wwc/workers/email_agent/agent.py
+++ wwc/workers/email_agent/agent.py
@@ -1,12 +1,12 @@
 from langgraph.graph import StateGraph, START, END
 from wwc.workers.email_agent.utils.state import EmailManagerState
-from wwc.workers.email_agent.utils.nodes import email_agent_node
+from wwc.workers.email_agent.utils.nodes import email_agent_node, human_node
 from langgraph.checkpoint.memory import MemorySaver
 
 email_agent_builder = StateGraph(EmailManagerState)
 email_agent_builder.add_node("email_agent_node", email_agent_node)
+email_agent_builder.add_node("human_node", human_node)
 email_agent_builder.add_edge(START, "email_agent_node")
-email_agent_builder.add_edge("email_agent_node", END)
 
 checkpointer = MemorySaver()
 email_agent = email_agent_builder.compile(checkpointer=checkpointer, name="email_agent")
--- wwc/workers/email_agent/utils/nodes.py
+++ wwc/workers/email_agent/utils/nodes.py
@@ -1,5 +1,5 @@
 from typing import Literal
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, ToolMessage
 from langgraph.types import Command
 from langchain_openai import ChatOpenAI
 from langgraph.prebuilt import create_react_agent
@@ -12,25 +12,33 @@ from wwc.workers.email_agent.utils.tools import get_user_input
 llm = ChatOpenAI(model="gpt-4o")
 
 # # human node
-# def email_human_node(state: EmailManagerState) -> Command[Literal["email_manager_node"]]:
-#     # get user input
-#     user_input = interrupt("Enter your input: ")
-#     # return user input
-#     return Command(update={"messages": [HumanMessage(content=user_input)]}, goto="email_manager_node")
+def human_node(state: EmailManagerState) -> Command[Literal["email_agent_node"]]:
+    # get user input
+    user_input = interrupt("Enter your input: ")
+    # return user input
+    return Command(update={"messages": [HumanMessage(content=user_input)]}, goto="email_agent_node")
 
 def email_agent_node(state: EmailManagerState):
     email_manager_instruction = """
-    You are an expert email manager. Your task is to help the user manage their email, inlcuding searching and sending.
-    If you need any information to help the user, you are only allowed to use the get_user_input tool, nothing else, with the question.
+    You are an expert email manager. Your task is to help the user manage their email, inlcuding searching and sending emails.
+    You will ask the user for all the necessary information to search for emails or send emails.
     Be professional, concise, and user-friendly. Ensure your final response follows the exact format for automated parsing.
     """
     email_manager_agent = create_react_agent(
         llm,
-        tools=tools + [get_user_input],
+        tools=tools,
         prompt=email_manager_instruction,
     )
     
     print("state['messages']: ", state['messages'])
     result = email_manager_agent.invoke({"messages": state['messages']})
-    print("result: ", result)
-    return Command(update={"messages": result["messages"]}, goto="__end__")
+    
+    # check for tool calls, if tool call, then goto end else goto human node
+    llm_message = result["messages"][-1]
+    if isinstance(llm_message, ToolMessage):
+        return Command(update={"messages": result["messages"]}, goto="__end__")
+    else:
+        return Command(update={"messages": result["messages"]}, goto="human_node")
+    
+
+    
