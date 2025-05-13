e73843bdd780f0c2a67f85911016e059aa5ab7ea
neocarb <external@neocarbtechnology.com>
2025-05-13 15:27:43 +0800

    add print for debug

--- wwc/workers/email_agent/utils/nodes.py
+++ wwc/workers/email_agent/utils/nodes.py
@@ -13,12 +13,13 @@ llm = ChatOpenAI(model="gpt-4o")
 
 # # human node
 def human_node(state: EmailManagerState) -> Command[Literal["email_agent_node"]]:
-    # get user input
+    print("in human_node")
     user_input = interrupt("Enter your input: ")
-    # return user input
+    print("user_input: ", user_input)
     return Command(update={"messages": [HumanMessage(content=user_input)]}, goto="email_agent_node")
 
 def email_agent_node(state: EmailManagerState) -> Command[Literal["__end__", "human_node"]]:
+    print("in email_agent_node")
     email_manager_instruction = """
     You are an expert email manager. Your task is to help the user manage their email, inlcuding searching and sending emails.
     You will ask the user for all the necessary information to search for emails or send emails.
@@ -33,9 +34,10 @@ def email_agent_node(state: EmailManagerState) -> Command[Literal["__end__", "hu
     print("state['messages']: ", state['messages'])
     result = email_manager_agent.invoke(state)
     
-    # check for tool calls, if tool call, then goto end else goto human node
-    llm_message = result["messages"][-1]
-    if isinstance(llm_message, ToolMessage):
+    # check for tool calls by email agent, if tool call, then goto end else goto human node
+    tool_message = next((msg for msg in result['messages'] if isinstance(msg, ToolMessage) and msg.name.contains("mail")), None)
+    print("tool_message: ", tool_message)
+    if tool_message:
         return Command(update={"messages": result["messages"]}, goto="__end__")
     else:
         return Command(update={"messages": result["messages"]}, goto="human_node")
